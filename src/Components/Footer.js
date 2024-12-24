a8ef464f53900d4b3cbd822a465c18b58047a88e
azerengazzou <engazzouazer1@gmail.com>
2024-12-24 19:07:35 +0100

    add rel to _blank targets

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {FaGithub, FaLinkedin} from "react-icons/fa";
 import {GrMail} from "react-icons/gr";
-import {SiLeetcode} from "react-icons/si"
 
 const Footer = () => {
   return (
@@ -9,9 +8,9 @@ const Footer = () => {
       <h4>Developed by Azer Engazzou</h4>
       <h4>Copyright &copy; 2024 AE</h4>
       <div className='footerLinks'>
-        <a href="https://github.com/azerengazzou" target='_blank'><FaGithub/></a>
-        <a href="https://www.linkedin.com/in/azer-engazzou/" target='_blank'><FaLinkedin/></a>
-        <a href='mailTo:engazzouazer1@gmail.com' target='_blank'><GrMail/></a>
+        <a href="https://github.com/azerengazzou" target='_blank'  rel="noreferrer"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/azer-engazzou/" target='_blank' rel="noreferrer"><FaLinkedin/></a>
+        <a href='mailTo:engazzouazer1@gmail.com' target='_blank' rel="noreferrer"><GrMail/></a>
       </div>
     </footer>
   )
